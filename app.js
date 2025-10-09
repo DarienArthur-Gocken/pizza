f570bb378afc37b865c0255592028549ac526088
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-10-09 10:52:25 -0700

    static file serving

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 
-    res.send('Welcome to Poppa\'s Pizza!');
+    res.send(`${import.meta.dirname}/views/home.html`);
 });
 
 app.listen(PORT, () => {
