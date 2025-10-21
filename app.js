5f13a17b9856935cbbf5919273b227ece92be320
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-10-21 10:59:26 -0700

    routes

--- app.js
+++ app.js
@@ -6,9 +6,35 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
+// form data in req.body
+app.use(express.urlencoded({ extended: true }));
+
+const orders = [];
+
 app.get('/', (req, res) => {
 
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.get('/contact-us', (req,res) => {
+
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+});
+
+app.get('/confirm', (req,res) => {
+
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+app.get('/admin', (req,res) => {
+
+    res.sendFile(`${import.meta.dirname}/views/admin.html`);
+});
+
+app.post('/submit-order', (req,res) => {
+
+    console.log(req.body);
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
 });
 
 app.listen(PORT, () => {
