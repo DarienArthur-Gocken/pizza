3786b3d209b1fab98fd00d9e327e6b4f3cdb6eae
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-11-18 10:33:19 -0800

    database

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
 
 const app = express();
 
@@ -11,7 +13,49 @@ app.use(express.static('public'));
 // form data in req.body
 app.use(express.urlencoded({ extended: true }));
 
-const orders = [];
+dotenv.config();
+
+const pool = mysql2.createPool({
+
+
+    host: process.env.DB_HOST,
+
+
+    user: process.env.DB_USER,
+
+
+    password: process.env.DB_PASSWORD,
+
+
+    database: process.env.DB_NAME,
+
+
+    port: process.env.DB_PORT
+
+
+}).promise();
+
+app.get('/db-test', async(req, res) => {
+    try {
+
+
+        const [orders] = await pool.query('SELECT * FROM orders');
+
+
+        res.send(orders);
+
+
+    } catch(err) {
+
+
+        console.error('Database error:', err);
+
+
+    }
+
+
+});
+
 
 app.get('/', (req, res) => {
 
@@ -31,31 +75,63 @@ app.get('/confirm', (req,res) => {
    // res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
 });
 
-app.get('/admin', (req,res) => {
+app.get('/admin', async(req, res) => {
+    try {
 
-    res.send(orders);
-    //res.sendFile(`${import.meta.dirname}/views/admin.html`);
+
+        const [orders] = await 
+
+
+        pool.query('SELECT * FROM orders ORDER BY timestamp DESC');
+
+
+        res.render('admin', { orders });
+
+
+    } catch(err) {
+
+
+        console.error('Database error:', err);
+
+
+    }
+
+});
+    
+app.post('/submit-order', async (req,res) => {
+
+
+    // Create a JSON object to store the data
+    const order = req.body;
+    order.timestamp = new Date()
+
+    // Write a query to insert order into DB
+    const sql = "INSERT INTO orders (fname, lname, email, size, method, toppings, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    console.log(order);
+    // Create array of parameters for each placeholder
+    const params = [
+        order.fname,
+        order.lname,
+        order.email,
+        order.size,
+        order.method,
+        order.toppings,
+        order.timestamp
+    ];
+
+    try {
+        const [result] = await pool.execute(sql, params);
+        
+        // Send user to confirmation page
+        res.render('confirmation', { order });
+    } catch(err) {
+        console.log("Database Error")
+    }
+
+
 });
 
-app.post('/submit-order', (req,res) => {
-
-    const order = {
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        method: req.body.method,
-        toppings: req.body.toppings, 
-        size: req.body.size,
-        comment: req.body.comment,
-        timestamp: new Date()
-    };
-
-    orders.push(order);
-    console.log(orders);
-
-    res.render('confirmation', { order: order});
-    //res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
